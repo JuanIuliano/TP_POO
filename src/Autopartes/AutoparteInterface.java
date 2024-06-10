85b453ed92a26da9e439a8de8c53e8549c2a6d91
Tomás Matías Lecuenis <112714402+tlecuenis@users.noreply.github.com>
2024-06-10 17:59:19 -0300

    Delete src directory

--- src/Autopartes/AutoparteInterface.java
+++
@@ -1,9 +0,0 @@
-package Autopartes;
-
-public interface AutoparteInterface {
-	void modificarDatos();
-	void mostrarAutoparte();
-	void borrarAutoparte(Autoparte autoparte);
-	void restarStock(Autoparte autoparte);
-	void sumarStock(Autoparte autoparte);
-}
